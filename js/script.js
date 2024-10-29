18fa2c6942774f022a5f0d7882079049d805a4f7
thalesluiz45 <thalesluiz45@hotmail.com>
2024-10-29 00:08:12 -0300

    Script update

--- js/script.js
+++ js/script.js
@@ -15,8 +15,18 @@ form.addEventListener('submit', function(event){
 
   if(bmi < 18.5){
     description = "Cuidado! Você está abaixo do peso!"
+  } else if(bmi >= 18.55 && bmi <= 25 ){
+    description = "Você está no peso ideal!"
+  } else if (bmi > 25 && bmi <= 30){
+    description = "Cuidado! Você está com sobrepeso!"
+  } else if (bmi >30 && bmi <=35){
+    description = "Cuidado! Você está com obesidade moderada!"
+  } else if (bmi > 35 && bmi <= 40) {
+    description = "Cuidado! Você está com obesidade severa!"
+  } else {
+    description = "Cuidado! Você está com obesidade morbida!"
   }
 
-  value.textContent = bmi;
+  value.textContent = bmi.replace('.',',');
   document.getElementById('descricao').textContent = description
 })
